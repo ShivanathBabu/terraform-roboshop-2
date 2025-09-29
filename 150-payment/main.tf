a5f2165a73c98370a699736c0b930919d1ea7028
ShivanathBabu <shivanathbabu.kodimyala@gmail.com>
2025-09-29 13:58:14 +0530

    terraform

--- 150-payment/main.tf
+++ 150-payment/main.tf
@@ -1,5 +1,5 @@
 module "payment" {
-  source = "git::https://github.com/ShivanathBabu/component-modules.git?ref:main"
+  source = "git::https://github.com/ShivanathBabu/component-modules.git?ref=main"
   rule_priority = 60
   component = "payment"
 }
